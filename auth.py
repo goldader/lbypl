f38ca12922be43e8a3cbb34bdef13896ce478a77
goldader <goldader@gmail.com>
2018-02-20 14:59:28 +0000

    Update of User

--- auth.py
+++ auth.py
@@ -227,7 +227,7 @@ def access_token(provider_id):
     c.execute("select r_lasttime, r_sec, a_token from accounts where user_id=? and provider_id=?", (Auth.uid, provider_id))
     v=(c.fetchone())
     r_lasttime=v[0]
-    r_sec=v[1]-3550 # use an expiry somewhat shorter in case processing time expires a token prior to using it
+    r_sec=v[1]-120 # use an expiry somewhat shorter in case processing time expires a token prior to using it
     # set expiry value
     expiry=timedelta(seconds=r_sec)+datetime.strptime(r_lasttime.split(".")[0],'%Y-%m-%d %H:%M:%S')
     # compare now to the expiry to determine if the old token is usable or a new one is required
@@ -237,6 +237,7 @@ def access_token(provider_id):
         refresh(provider_id)
         c.execute("select a_token from accounts where user_id=? and provider_id=?", (Auth.uid, provider_id))
         return(c.fetchone()[0])
+
 
 """
 Auth("goldader@gmail.com")
