108bd6eb6d413873bdd9f88d9f56f499c0501085
goldader <35690563+goldader@users.noreply.github.com>
2018-02-27 11:43:07 +0000

    Delete tl_access.py

--- tl_access.py
+++
@@ -1,8 +0,0 @@
-class TL_access(object):
-    """Providers TL credentials for API access"""
-
-    client_id="goldader-znsm"
-    client_secret="xlbucdespuhv4zsdf8y2z"
-
-    def __init__(self):
-        pass
